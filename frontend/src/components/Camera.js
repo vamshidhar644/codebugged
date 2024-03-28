8e692d42738d3d018da5458522cf3c300cfd5117
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2024-03-28 13:55:15 +0530

    Face detecting

--- frontend/src/components/Camera.js
+++ frontend/src/components/Camera.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import * as faceapi from 'face-api.js';
 
 const Camera = () => {
-  return (
-    <div>Camera</div>
-  )
-}
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
-export default Camera
+  useEffect(() => {
+    const loadModels = async () => {
+      await Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      ]);
+      startVideo();
+    };
+
+    loadModels();
+  }, []);
+
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: {} })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    if (videoRef.current) {
+      const video = videoRef.current;
+
+      video.addEventListener('play', () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+          // console.log(displaySize);
+
+          setInterval(async () => {
+            const displaySize = { width: video.width, height: video.height };
+
+            faceapi.matchDimensions(canvas, displaySize);
+            const detections = await faceapi
+              .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+              .withFaceExpressions();
+            const resizedDetections = faceapi.resizeResults(
+              detections,
+              displaySize
+            );
+
+            canvas
+              .getContext('2d')
+              .clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+          }, 10);
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <div className="relative">
+      <video ref={videoRef} width="720" height="560" autoPlay muted />
+      <canvas ref={canvasRef} style={{ position: 'absolute', top: 0 }} />
+    </div>
+  );
+};
+
+export default Camera;
